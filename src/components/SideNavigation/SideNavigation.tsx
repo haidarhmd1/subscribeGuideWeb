c7c4c86866b28db2337c46dfcf94012a3e3267d1
haidarhmd1 <haidar.hmd1@gmail.com>
2023-09-06 13:42:18 +0200

    alter sideNavigation to detect browser width change for mobile

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 
 import { Divider } from "antd";
@@ -5,9 +7,13 @@ import { Divider } from "antd";
 import { FcTreeStructure } from "react-icons/fc";
 import { FiSettings } from "react-icons/fi";
 import { MdOutlineSubscriptions } from "react-icons/md";
+import { useWindowSize } from "utils/useWindowSize";
 
 export const SideNavigation = () => {
-  return (
+  const { width: windowWidth } = useWindowSize();
+  const isBrowserMobile = !!windowWidth && windowWidth < 500;
+
+  const SideNav = !isBrowserMobile ? (
     <section className="side-nav w-34 h-screen border-r-[1px] border-gray-200 sticky top-0">
       <div className="side-nav-logo h-12 flex justify-evenly items-center">
         <FcTreeStructure size={23} />
@@ -32,5 +38,7 @@ export const SideNavigation = () => {
         </Link>
       </div>
     </section>
-  );
+  ) : null;
+
+  return SideNav;
 };
