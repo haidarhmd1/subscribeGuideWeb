30a68bac562f81bbfd41ea3fffc9a141266ed0cb
haidarhmd1 <haidar.hmd1@gmail.com>
2023-09-13 14:02:25 +0200

    add the necessary main-content div into the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Space, Drawer, Tabs, ConfigProvider } from "antd";
+import { Space, Drawer, ConfigProvider } from "antd";
 import { PrimaryButton, SecondaryButton } from "components/Button";
 
 import { TbCirclePlus } from "react-icons/tb";
@@ -14,13 +14,14 @@ import { SubscriptionModal } from "components/SubscriptionModal";
 import { SubscriptionForm } from "components/SubscriptionForm";
 import { SubscriptionCards } from "components/SubscriptionCards/SubscriptionCards";
 import theme from "theme/themeConfig";
+import { MOBILE_BREAKPOINT } from "utils/constants";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const { width: windowWidth } = useWindowSize();
-  const isBrowserMobile = !!windowWidth && windowWidth < 500;
+  const isBrowserMobile = !!windowWidth && windowWidth < MOBILE_BREAKPOINT;
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -39,37 +40,43 @@ export default function Home() {
   };
 
   return (
-    <ConfigProvider theme={theme}>
-      <PrimaryButton onClick={openModal}>
-        <div className="flex">
-          <TbCirclePlus className="mr-2" size={16} />
-          Add Subscriptions
-        </div>
-      </PrimaryButton>
-      <SubscriptionModal isModalOpen={isModalOpen} closeModal={closeModal} />
-      <SubscriptionMainAnalytics
-        incomingSpendings="$16.00"
-        moneySpentTillNow="$31.00"
-      />
-      <SubscriptionCards showDrawer={showDrawer} />
-      <PaymentHistory />
-      <Drawer
-        title={isBrowserMobile ? "" : "Subscription"}
-        height={500}
-        width={600}
-        onClose={onCloseDrawer}
-        placement={isBrowserMobile ? "bottom" : "right"}
-        open={drawerOpen}
-        bodyStyle={{ paddingBottom: isBrowserMobile ? 0 : 80 }}
-        extra={
-          <Space>
-            <SecondaryButton onClick={onCloseDrawer}>Cancel</SecondaryButton>
-            <PrimaryButton onClick={onCloseDrawer}>Submit</PrimaryButton>
-          </Space>
-        }
-      >
-        <SubscriptionForm />
-      </Drawer>
-    </ConfigProvider>
+    <div
+      className={`main-content p-2.5  bg-gray-100 ${
+        isBrowserMobile ? "pb-14" : ""
+      }`}
+    >
+      <ConfigProvider theme={theme}>
+        <PrimaryButton onClick={openModal}>
+          <div className="flex">
+            <TbCirclePlus className="mr-2" size={16} />
+            Add Subscriptions
+          </div>
+        </PrimaryButton>
+        <SubscriptionModal isModalOpen={isModalOpen} closeModal={closeModal} />
+        <SubscriptionMainAnalytics
+          incomingSpendings="$16.00"
+          moneySpentTillNow="$31.00"
+        />
+        <SubscriptionCards showDrawer={showDrawer} />
+        <PaymentHistory />
+        <Drawer
+          title={isBrowserMobile ? "" : "Subscription"}
+          height={500}
+          width={600}
+          onClose={onCloseDrawer}
+          placement={isBrowserMobile ? "bottom" : "right"}
+          open={drawerOpen}
+          bodyStyle={{ paddingBottom: isBrowserMobile ? 0 : 80 }}
+          extra={
+            <Space>
+              <SecondaryButton onClick={onCloseDrawer}>Cancel</SecondaryButton>
+              <PrimaryButton onClick={onCloseDrawer}>Submit</PrimaryButton>
+            </Space>
+          }
+        >
+          <SubscriptionForm />
+        </Drawer>
+      </ConfigProvider>
+    </div>
   );
 }
