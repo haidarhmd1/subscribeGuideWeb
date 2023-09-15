19a7e15b422149a0a44d5bd40d3b5a4c25c80835
haidarhmd1 <haidar.hmd1@gmail.com>
2023-09-15 17:26:35 +0200

    add popover tray under the notifications and adjust according to mobile responsive, as well add formatted date in the header

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,47 +1,50 @@
 "use client";
 
-import { Avatar, Dropdown, Space } from "antd";
-import { items } from "./MainHeader.helper";
-import { FiBell, FiCalendar, FiMail } from "react-icons/fi";
+import { Avatar, Dropdown, Popover, Space } from "antd";
+import { items, useListContent } from "./MainHeader.helper";
+import { FiBell, FiCalendar } from "react-icons/fi";
+import { formattedDate } from "utils/getFormattedDate";
 
 export const MainHeader = () => {
+  const { content } = useListContent();
+
   return (
-    <div className="main-header h-12 w-full border-b-[1px] border-gray-200 p-2 flex justify-end">
+    <div className="main-header h-12 w-full border-b-[1px] border-gray-200 p-2 flex">
       <div className="flex text-xs py-2 px-2 mr-2 text-black rounded-md border-0 font-light bg-gray-100">
         <FiCalendar size={14} />
         <span className="text-xs font-bold ml-2">Date:</span>
-        <span className="ml-1">23 March 2023</span>
+        <span className="ml-1">{formattedDate}</span>
       </div>
-      <div className="p-2 bg-gray-200 rounded-md">
-        <FiBell
-          size={16}
-          style={{
-            alignSelf: "center",
-          }}
-        />
-      </div>
+      <div className="flex ml-auto">
+        <Popover
+          placement="bottomLeft"
+          title="Notifications"
+          content={content}
+          trigger="click"
+        >
+          <div className="cursor-pointer p-2 bg-gray-200 rounded-md mr-2">
+            <FiBell
+              size={16}
+              style={{
+                alignSelf: "center",
+              }}
+            />
+          </div>
+        </Popover>
 
-      <div className="p-2 bg-gray-200 rounded-md mx-3">
-        <FiMail
-          size={16}
-          style={{
-            alignSelf: "center",
-          }}
-        />
+        <Dropdown menu={{ items }} trigger={["click"]}>
+          <a onClick={(e) => e.preventDefault()}>
+            <Space>
+              <Avatar
+                className="cursor-pointer"
+                style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+              >
+                HH
+              </Avatar>
+            </Space>
+          </a>
+        </Dropdown>
       </div>
-
-      <Dropdown menu={{ items }} trigger={["click"]}>
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            <Avatar
-              className="cursor-pointer"
-              style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
-            >
-              HH
-            </Avatar>
-          </Space>
-        </a>
-      </Dropdown>
     </div>
   );
 };
